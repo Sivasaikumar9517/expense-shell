b842c33e5313018c10c0d043420b962828061b4b
sivasaikum <sivasaikumarallampati@gmail.com>
2025-12-04 10:04:29 +0530

    expense-mysql

--- backend.sh
+++ backend.sh
@@ -29,47 +29,47 @@ fi
 
 echo "Script started executing at: $TIME_STAMP"
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$LOG_FILE_NAME
 VALIDATE $? "Disabling the default node js version"
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>$LOG_FILE_NAME
 VALIDATE $? "enabling node js version 20"
 
-dnf install nodejs -y
+dnf install nodejs -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing the nodejs"
 
-useradd expense
+useradd expense &>>$LOG_FILE_NAME
 VALIDATE $? "Adding the expense user "
 
-mkdir /app
+mkdir /app &>>$LOG_FILE_NAME
 VALIDATE $? "Creating App directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE_NAME
 VALIDATE $? "Downloading the code"
 
 cd /app
 
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip &>>$LOG_FILE_NAME
 VALIDATE $? "Unzipping the code into app directory"
 
-npm install
+npm install &>>$LOG_FILE_NAME 
 VALIDATE $? "Installing the dependenciecs"
 
 cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
 
-dnf install mysql -y
+dnf install mysql -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing Mysql client"
 
-mysql -h mysql.jobsearchindia.online -u root -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h mysql.jobsearchindia.online -u root -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE_NAME
 VALIDATE $? "Loading the schema"
 
-systemctl daemon-reload
+systemctl daemon-reload &>>$LOG_FILE_NAME
 VALIDATE $? "Daemon reload is "
 
-systemctl enable backend
+systemctl enable backend &>>$LOG_FILE_NAME
 VALIDATE $? "enablling backend"
 
-systemctl restart backend
+systemctl restart backend &>>$LOG_FILE_NAME
 VALIDATE $? "restart the backend"
 
 
